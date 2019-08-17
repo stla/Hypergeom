95fea9d617124397f61428c0d2b1566fb3c6b407
stla <laurent_step@yahoo.fr>
2019-08-17 19:35:24 +0200

    dico

--- src/Hypergeom.hs
+++ src/Hypergeom.hs
@@ -2,7 +2,11 @@
 module Hypergeom where
 import           Data.Array
 import           Math.Combinat.Partitions.Integer.IntList (_dualPartition,
-                                                           _isPartition)
+                                                           _isPartition,
+                                                           _partitionsWithKParts)
+import Data.Sequence (Seq, update, (|>))
+import qualified Data.Sequence as S
+import Data.List (nub, sort, sortOn, findIndex, elemIndex)
 
 _betaratio :: Fractional a => [Int] -> [Int] -> Int -> a -> a
 _betaratio kappa mu k alpha = alpha * prod1 * prod2 * prod3
@@ -60,3 +64,75 @@ hypergeoI m alpha a b n x =
         then s + summation (i+1) z' (j-kappai) kappa'
         else s
       s'' = s' + z'
+
+dico :: Int -> Int -> [Maybe Int]
+dico m n = map (\x -> elemIndex (x ++ [1]) parts') (filter (\y -> length y < n) parts')
+  where
+  parts =
+     nub $ concatMap (\i -> (concatMap (\j -> _partitionsWithKParts (min j i) i) [1 .. n] )) [1 .. m]
+  parts' = sortOn length $ sort parts
+      -- D <- rep(NA_integer_, Pmn)
+      -- Last <- t(as.integer(c(0,m,m))) # il faut m > n (sinon l = 0 et Last est vide)
+      -- fin <- 0L
+      -- for(. in 1L:n){
+      --   NewLast <- matrix(NA_integer_, nrow = 0L, ncol = 3L)
+      --   for(i in 1L:nrow(Last)){
+      --     manque <- Last[i,2L]
+      --     l <- min(manque, Last[i,3L])
+      --     if(l > 0L){
+      --       D[Last[i,1L]+1L] <- fin + 1L
+      --       s <- 1L:l
+      --       NewLast <- rbind(NewLast, cbind(fin+s, manque-s, s))
+      --       fin <- fin + l
+      --     }
+      --   }
+      --   Last <- NewLast
+      -- }
+-- dico' :: Int -> Int -> Seq (Maybe Int)
+-- dico' m n = go 1 [0] [m] [m] 0 (S.fromList (replicate 15 Nothing))
+--   where
+--   go :: Int -> [Int] -> [Int] -> [Int] -> Int -> Seq Int -> Seq Int
+--   go k a' b' c' end' d'
+--     | k == n = d'
+--     | otherwise = inner 0 a' b' c' end' d'
+--       where
+--       inner :: Int -> [Int] -> [Int] -> [Int] -> Int -> Seq Int -> Seq Int
+--       inner i a b c end d
+--         | i == length a = d
+--         | otherwise = if b!!i > 0
+--           then let l = min ((b!!i) (c!!i)) in
+--           let dd = update (a!!i) (end+1) d in
+--           inner (i+1) (a ++ [end + 1 .. end + l]) (b ++ map (\x -> b!!i - x) [1 .. l]) (c ++ [1 .. l]) (end +l) dd
+--           else inner (i+1) a b c end d
+-- dico' :: Int -> Int -> Seq (Maybe Int)
+-- dico' m n = go 1 (S.fromList (replicate 15 Nothing)) -- 15 to be replaced
+--   where
+--   go :: Int -> Seq (Maybe Int) -> Seq (Maybe Int)
+--   go k d'
+--     | k == n-1 = d'
+--     | otherwise = go (k+1) (inner 0 [0] [m] [m] 0 d')
+--       where
+--       inner :: Int -> [Int] -> [Int] -> [Int] -> Int -> Seq (Maybe Int) -> Seq (Maybe Int)
+--       inner i a b c end d
+--         | i == length a = d
+--         | otherwise = if b!!i > 0
+--           then let l = min (b!!i) (c!!i) in
+--           let dd = update (a!!i) (Just $ end+1) d in
+--           inner (i+1) (a ++ [end + 1 .. end + l]) (b ++ map (\x -> b!!i - x) [1 .. l]) (c ++ [1 .. l]) (end + l) dd
+--           else inner (i+1) a b c end d
+dico' :: Int -> Int -> Seq (Maybe Int)
+dico' m n = go 1 S.empty
+  where
+  go :: Int -> Seq (Maybe Int) -> Seq (Maybe Int)
+  go k d'
+    | k == n-1 = d'
+    | otherwise = go (k+1) (inner 0 [0] [m] [m] 0 d')
+      where
+      inner :: Int -> [Int] -> [Int] -> [Int] -> Int -> Seq (Maybe Int) -> Seq (Maybe Int)
+      inner i a b c end d
+        | i >= length a = d
+        | otherwise = if b!!i > 0
+          then let l = min (b!!i) (c!!i) in
+          let dd = d |> (Just $ end+1) in
+          inner (i+1) (a ++ [end + 1 .. end + l]) (b ++ map (\x -> b!!i - x) [1 .. l]) (c ++ [1 .. l]) (end + l) dd
+          else inner (i+1) a b c end (d |> Nothing)
